7fdaecc1aceee179532b1c8c710a8aabef35b5be
Hass <hassmroueh@gmail.com>
2024-12-04 01:19:50 -0500

    dsa- set- coding complete

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,27 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+    
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self.dictionary
+    def delete(self, value):
+        self.dictionary.pop(value,None)
+        return self.dictionary
+    def size(self):
+        return len(self.dictionary)
+    def clear(self):
+        self.dictionary.clear()
 
-    pass
+    def __str__(self):
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
+
+set = MySet([1,2,3,3])
